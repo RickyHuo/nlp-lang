f95552ca6920710d817eaa9081ec15d7a174c6d2
RickyHuo <huochen1994@163.com>
2019-03-18 16:48:03 +0800

    Support polyphone;

--- src/main/java/org/nlpcn/commons/lang/pinyin/PinyinUtil.java
+++ src/main/java/org/nlpcn/commons/lang/pinyin/PinyinUtil.java
@@ -113,29 +113,59 @@ enum PinyinUtil {
 
 		String temp = null;
 		int beginOffe = 0;
+        List<List<String>> intraLists = new LinkedList<>();
 		while ((temp = word.getFrontWords()) != null) {
+			if (beginOffe != word.offe) {
+			    for (List<String> list : intraLists) {
+                    lists.add(null);
+			        lists.addAll(list);
+                }
+                intraLists = new LinkedList<>();
+            }
 
-			for (int i = beginOffe; i < word.offe; i++) {
-				lists.add(null);
-			}
+            String[] param = word.getParam();
+			int intraSize = intraLists.size();
+            if (intraSize > 0) {
+                if (param.length > 1) {
+                    for (int i=0; i<intraSize; i++) {
+                        List<String> list = new LinkedList<String>();
+                        list.addAll(intraLists.get(i));
 
-			if(temp.length()==1){ //单个字取第一个拼音
-				lists.add(PinyinFormatter.formatPinyin(word.getParam()[0], format));
-			}else{
-				for (String t : word.getParam()) {
-					lists.add(PinyinFormatter.formatPinyin(t, format));
+                        for (int j = 0; j < param.length; j ++) {
+                            list.add(PinyinFormatter.formatPinyin(param[j], format));
+                            intraLists.add(((LinkedList<String>) ((LinkedList<String>) list).clone()));
+                            list.remove(list.size() - 1);
+                        }
 
-				}
-			}
-
-			beginOffe = word.offe + temp.length();
+                    }
+                    for (int i=0; i<intraSize; i++) {
+                        intraLists.remove(intraSize-i-1);
+                    }
+                } else {
+                    for (int i = 0; i<intraLists.size(); i++) {
+                        intraLists.get(i).add(PinyinFormatter.formatPinyin(param[0], format));
+                    }
+                }
+            } else {
+                if (param.length > 1) {
+                    for (int j = 0; j < param.length; j ++) {
+                        LinkedList<String> newList  = new LinkedList<String>();
+                        newList.add(PinyinFormatter.formatPinyin(param[j], format));
+                        intraLists.add(newList);
+                    }
+                } else {
+                    LinkedList<String> newList  = new LinkedList<String>();
+                    newList.add(PinyinFormatter.formatPinyin(param[0], format));
+                    intraLists.add(newList);
+                }
+            }
+            beginOffe = word.offe + temp.length();
 		}
 
-		if (beginOffe < str.length()) {
-			for (int i = beginOffe; i < str.length(); i++) {
-				lists.add(null);
-			}
-		}
+        for (List<String> list : intraLists) {
+            lists.addAll(list);
+            lists.add(null);
+        }
 
 
 		return lists;
